1a95772e92171643a2077e6ce276c0f6a62bf705
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2012-07-21 10:22:51 +0900

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'c767c4eb8eaf92ac2b012cf114dc771a3062054e565729952233243ed5fc2212661dbf0362d1eb601d819cd19c546cac72da8ba00df0003e91744fa5e211b0ae'
